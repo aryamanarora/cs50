e587bd644bf6ed61bf0ae7980d05f97bc2a9bd7f
aryamanarora <aryaman.arora2020@gmail.com>
2016-09-18 18:04:53 -0400

    create initials.c

---
+++ Week 2/initials.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+    // prompts for name, gets length
+    string name = GetString();
+    int namelength = strlen(name);
+
+    // first letter is always an initial; make the first letter uppercase
+    char firstinitial = toupper(name[0]);
+    // print first letter of the name
+    printf("%c", firstinitial);
+
+    // scan through letters until a space is found, then prints the next letter in uppercase
+    for (int i = 0; i < namelength; i++)
+    {
+        if (name[i] == 32)
+        {
+            char initial = toupper(name[i+1]);
+            printf("%c",initial);
+        }
+    }
+
+    //newline
+    printf("\n");
+}
