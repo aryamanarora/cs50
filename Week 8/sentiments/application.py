498ec5f6b9a732eb71a249f53314ed8eb9361406
aryamanarora <aryaman.arora2020@gmail.com>
2017-01-21 23:02:37 -0500

    done with pset6

--- Week 8/sentiments/application.py
+++ Week 8/sentiments/application.py
@@ -1,13 +1,17 @@
 from flask import Flask, redirect, render_template, request, url_for
 
+import sys
+import os
 import helpers
 from analyzer import Analyzer
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/search")
 def search():
@@ -18,10 +22,35 @@ def search():
         return redirect(url_for("index"))
 
     # get screen_name's tweets
-    tweets = helpers.get_user_timeline(screen_name)
+    tweets = helpers.get_user_timeline(screen_name, 100)
 
-    # TODO
+    # absolute paths to lists
+    positives = os.path.join(sys.path[0], "positive-words.txt")
+    negatives = os.path.join(sys.path[0], "negative-words.txt")
+
+    # classify tweets
     positive, negative, neutral = 0.0, 0.0, 100.0
+    positive_count, negative_count, neutral_count = 0, 0, 0
+
+    if len(tweets) != 0:
+
+        # instantiate analyzer
+        analyzer = Analyzer(positives, negatives)
+
+        # analyze tweets
+        for tweet in tweets:
+            score = analyzer.analyze(tweet)
+            if score > 0.0:
+                positive_count += 1
+            elif score < 0.0:
+                negative_count += 1
+            else:
+                neutral_count += 1
+
+        # get percentages
+        positive, negative, neutral = positive_count / len(tweets), \
+            negative_count / len(tweets), \
+            neutral_count / len(tweets)
 
     # generate chart
     chart = helpers.chart(positive, negative, neutral)
