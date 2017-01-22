e67bebb0697b18439dc56011d689cd06ee20f7cd
aryamanarora <aryaman.arora2020@gmail.com>
2017-01-21 21:53:39 -0500

    Finish analyzer for real

--- Week 8/sentiments/analyzer.py
+++ Week 8/sentiments/analyzer.py
@@ -1,4 +1,4 @@
-from nltk import word_tokenize
+import nltk
 
 
 class Analyzer():
@@ -7,19 +7,31 @@ class Analyzer():
     def __init__(self, positives, negatives):
         """Initialize Analyzer."""
 
-        self.pos = [line.rstrip('\n') for line in open(positives)]
-        self.neg = [line.rstrip('\n') for line in open(negatives)]
+        # store positive and negative words
+        # from files, stripping newlines
+        # and ignoring blanklines
+        self.pos = [line.rstrip('\n') for line in open(positives)
+                    if line.rstrip('\n')]
+        self.neg = [line.rstrip('\n') for line in open(negatives)
+                    if line.rstrip('\n')]
 
     def analyze(self, text):
         """Analyze text for sentiment, returning its score."""
 
-        tokenized_text = word_tokenize(text)
+        # tokenize text
+        tokens = nltk.tokenize.TweetTokenizer.tokenize(text)
+
+        # make a list of integers, where
+        # 1 is a positive word, -1 is a
+        # negative word, and 0 is neutral
         analysis = []
-        for word in tokenized_text:
+        for word in tokens:
             if word in self.pos:
                 analysis.append(1)
             elif word in self.neg:
                 analysis.append(-1)
             else:
                 analysis.append(0)
+
+        # sum the list's values for sentiment analysis
         return(sum(analysis))
